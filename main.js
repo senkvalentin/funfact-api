1b133b1fa16b91cb438122a7e3978676eb4e71d6
senkv <senkv@edu.htl-villach.at>
2023-11-22 14:47:41 +0100

    implementet api call with js fetch

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
 'use strict';
 let factField = document.getElementById('factField');
-factField.innerHTML = 'main.js';
+let generate = document.getElementById('generate');
+factField.innerHTML = 'Want to see a fun fact? Click on the button below to start.';
+
+
+
+var limit = 1;
+var apiKey = '---insert your key to start---';
+
+
+
+let url = 'https://facts-by-api-ninjas.p.rapidapi.com/v1/facts?limit=1';
+const options = {
+	method: 'GET',
+	headers: {'X-Api-Key': 'fNCqYl7drebTb/amokr7Ag==EYcTK0bKh1HUpBMS'},
+    contentType: 'application/json'
+};
+
+
+
+generate.onclick = function () {
+    factField.innerHTML = 'Loading...';
+    fetch('https://api.api-ninjas.com/v1/facts?limit=' + limit, {
+        method: 'GET',
+        headers: {
+            'X-Api-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(result => {
+            console.log(result);
+            factField.innerHTML = result[0].fact;
+        })
+        .catch(error => {
+            console.error('Error: ', error.message);
+        });
+};
